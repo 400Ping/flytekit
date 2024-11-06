3bd27168e520e3f08af335a3d356c53fed45c1ee
400Ping <43886578+400Ping@users.noreply.github.com>
2024-11-06 15:44:33 +0800

    add on failure parameter to imperative workflow
    Signed-off-by: 400Ping <43886578+400Ping@users.noreply.github.com>

--- flytekit/core/workflow.py
+++ flytekit/core/workflow.py
@@ -420,10 +420,11 @@ class ImperativeWorkflow(WorkflowBase):
     """
 
     def __init__(
-        self,
-        name: str,
-        failure_policy: Optional[WorkflowFailurePolicy] = None,
-        interruptible: bool = False,
+    self,
+    name: str,
+    failure_policy: Optional[WorkflowFailurePolicy] = None,
+    interruptible: bool = False,
+    on_failure: Optional[Union[WorkflowBase, Task]] = None,
     ):
         metadata = WorkflowMetadata(on_failure=failure_policy or WorkflowFailurePolicy.FAIL_IMMEDIATELY)
         workflow_metadata_defaults = WorkflowMetadataDefaults(interruptible)
